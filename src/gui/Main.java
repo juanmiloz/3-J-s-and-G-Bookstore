ff6c81ec9cd9ed0f62327b487277f07879c190b2
Juan Camilo Zorrilla <juanmiloz@hotmail.com>
2021-03-08 22:48:31 -0500

    added screen Store information

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("prueba.fxml"));
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("StoreSetup.fxml"));
 		fxmlLoader.setController(bookstoreGUI);
 		Parent root = fxmlLoader.load();
 		
