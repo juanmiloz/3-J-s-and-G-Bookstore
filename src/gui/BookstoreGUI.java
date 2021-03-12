cfa3efe2f1d4a30f30da4e6f6a0ba21b774eb812
Juan Sanin <juanpablosaninb@gmail.com>
2021-03-12 16:37:55 -0500

    Store setup and store information GUI added

--- src/gui/BookstoreGUI.java
+++ src/gui/BookstoreGUI.java
@@ -1,6 +1,10 @@
 package gui;
 import java.io.IOException;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -9,36 +13,117 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.shape.Shape;
+import javafx.scene.text.Text;
 import model.Bookstore;
 
 public class BookstoreGUI {
-	
+
 	private Bookstore bookstore;
-	
+
 	//Attributes mainPane.fxml
 	@FXML
 	private BorderPane mainPane;
+
+
+	//Attributes StoreSetup.fxml
+
+	@FXML
+	private JFXTextField txtFieldCashiers;
+
+	@FXML
+	private JFXTextField txtFieldShelves;
+
+	@FXML
+	private JFXButton btnContinue;
 	
+	
+	//Attributes StoreInformation.fxml
+
+	@FXML
+	private JFXButton btnAddBooks;
+
+	@FXML
+	private JFXTextField txtFieldShelveName;
+
+	@FXML
+	private JFXTextField txtFieldNumBooks;
+
+	@FXML
+	private JFXButton btnAddToShelve;
+
+	@FXML
+	private JFXTextField txtFieldBookTitle;
+
+	@FXML
+	private JFXTextField txtFieldAuthor;
+
+	@FXML
+	private JFXTextField txtFieldStock;
+
+	@FXML
+	private JFXTextField txtFieldISBN;
+
+	@FXML
+	private JFXTextField txtFieldPrice;
+
+	@FXML
+	private Text txtCompShelves;
+
+	@FXML
+	private Text txtMaxShelves;
+
+	@FXML
+	private Text txtCompBooks;
+
+	@FXML
+	private Text txtMaxBooks;
+
+	@FXML
+	private JFXButton backButton;
+
+
 	public BookstoreGUI(Bookstore bookstore) {
 		this.bookstore = bookstore;
 	}
-	
+
 	public void showMainScreen() throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("StoreSetup.fxml"));
 		fxmlLoader.setController(this);
-		
+
 		Parent mainScreen = fxmlLoader.load();
-		
+
 		mainPane.getStyleClass().addAll(mainScreen.getStylesheets());
 		mainPane.getChildren().clear();
 		mainPane.setCenter(mainScreen);
 	}
 	
-	//Alerts
 	
+
+	@FXML
+	void btnPressedContinue(ActionEvent event) {
+
+	}
+	
+	@FXML
+    void addBooks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addToShelve(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
+	//Alerts
+
 	public void alertLoadProgram() {
 		Alert alert = new Alert(AlertType.ERROR);
-		
+
 		alert.setHeaderText("Imposible load the program");
 		alert.setContentText("Its imposible load the program, please contact your supplier");
 		alert.show();
