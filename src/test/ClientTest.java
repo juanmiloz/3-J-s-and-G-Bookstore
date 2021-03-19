6561c49b18495e8f5fa2b7bc7e181b4b4ba9d9ad
Juanserodriguez16 <juseros9@gmail.com>
2021-03-19 12:43:48 -0500

    test package

---
+++ src/test/ClientTest.java
@@ -0,0 +1,30 @@
+package test;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import model.*;
+
+public class ClientTest extends TestCase{
+	private Client client;
+	
+	@Test
+	public void setUpEscenary1() {
+		String name = "giovanni";
+		String id = "45432467";
+		int time = 0;
+		client = new Client(name, id, time);
+	}
+	public void test1() {
+		setUpEscenary1();
+		assertEquals("giovanni", client.getName());
+	}
+	public void test2() {
+		setUpEscenary1();
+		boolean x = false;
+		if (client != null) {
+			x = true;
+		}
+		assertTrue(x);
+		
+	}
+}
